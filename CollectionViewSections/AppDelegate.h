ba669144b8d6227ec489efc60a1b9796bf8e404b
akshay jain <akshayjain@akshays-macbook-pro.local>
2016-06-08 14:57:51 +0530

    Initial Commit

---
+++ CollectionViewSections/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  CollectionViewSections
+//
+//  Created by akshay jain on 08/06/16.
+//  Copyright © 2016 akshay jain. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ CollectionViewSections/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  CollectionViewSections
+//
+//  Created by akshay jain on 08/06/16.
+//  Copyright © 2016 akshay jain. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
